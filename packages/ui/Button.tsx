417d04e3b037ed2e2d541ffe62f572dc92c60d4d
Manjinder <msinghw3@gmail.com>
2023-10-13 23:55:09 -0400

    Initial commit with a new client application

---
+++ packages/ui/Button.tsx
@@ -0,0 +1,14 @@
+"use client";
+import * as React from "react";
+
+export const Button = () => {
+  return (
+    <button
+      onClick={() => {
+        alert("Boop");
+      }}
+    >
+      Boop
+    </button>
+  );
+};
