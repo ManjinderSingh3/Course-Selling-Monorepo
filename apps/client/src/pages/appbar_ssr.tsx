e3ea23dc8814c5735b6d3e48a13d81304eada45e
Manjinder <msinghw3@gmail.com>
2023-10-19 19:12:31 -0400

    Added server side rendering in a component

--- apps/client/src/pages/appbar_ssr.tsx
+++ apps/client/src/pages/appbar_ssr.tsx
@@ -1,16 +1,20 @@
+/*
+Notes: 
+1- If a component has two exports, other than default export of the component, the second export runs first.
+*/
+
 import { Typography, Button } from "@mui/material";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import HomeRoot from "@/components/HomeRoot";
-import { useSetRecoilState } from 'recoil';
-import { userState } from 'store';
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
 
-//  ---------  Currently this handler has logic of Appbar  ----------
-export default function Home() {
+export default function handler({ session }) {
+  // The session object passed in the parameter of handler is passed from getServerSideProps method
   const router = useRouter();
-  const session = useSession(); //This is a client side hook. It runs on client side and sends backend request to get user's details
-  //const setUser = useSetRecoilState(userState);
-  if (session.data) {
+
+  if (session.user) {
     return (
       <div>
         <div
@@ -51,7 +55,6 @@ export default function Home() {
               variant="contained"
               onClick={() => {
                 signOut();
-                //router.push("/");
               }}
             >
               Logout
@@ -80,43 +83,44 @@ export default function Home() {
         >
           <Typography variant="h5"> CourseHub</Typography>
         </div>
-        <div style={{ display: "flex" }}>
-          <div style={{ marginRight: 10, borderRadius: "20" }}>
-            <Button
-              onClick={() => {
-                router.push("/signin");
-              }}
-              style={{ color: "#0d9488" }}
-            >
-              Sign In
-            </Button>
-          </div>
-          <div>
-            <Button
-              variant="contained"
-              onClick={() => {
-                signIn();
-                console.log(session.data);
-                console.log("--Signup Successful !!");
-                // if(session.data){
-                //   setUser({
-                //     isLoading:false,
-                //     userEmail: session.data.user?.email
-                //   })
-                // }
-                router.push("/signin");
-              }}
-              style={{
-                borderRadius: 9999,
-                backgroundColor: "#0d9488",
-                fontSize: ".875rem",
-              }}
-            >
-              Signup
-            </Button>
-          </div>
+        <div>
+          <Button
+            variant="contained"
+            onClick={() => {
+              signIn();
+            }}
+            style={{
+              borderRadius: 9999,
+              backgroundColor: "#0d9488",
+              fontSize: ".875rem",
+            }}
+          >
+            Sign In
+          </Button>
         </div>
       </div>
     );
   }
+}
+
+// The control will first reach here. It will get the details of the user if they are already logged in or not
+
+// context.req contains cookies, and getServerSession extracts cookies from it
+export async function getServerSideProps(context) {
+  const session = await getServerSession(context.req, context.res, authOptions); // The session object will have deatils on/of the server
+
+  //If user is not logged in
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
 }
