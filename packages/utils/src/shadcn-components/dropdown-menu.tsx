9cdf960c1812d42103462613f1c981ae9349f3d0
Manjinder <msinghw3@gmail.com>
2023-11-07 20:29:47 -0500

    Bolier plate code for shadcn components

---
+++ packages/utils/src/shadcn-components/dropdown-menu.tsx
@@ -0,0 +1,4 @@
+import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
+import Link from "next/link";
+import { cn } from "../index";
+import { ChevronDown } from "lucide-react"
