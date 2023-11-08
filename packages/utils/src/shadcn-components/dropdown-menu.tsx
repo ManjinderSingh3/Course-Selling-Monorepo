ec7ce27a4a5faf603f0b88459ea7a4e3e83e2483
Manjinder <msinghw3@gmail.com>
2023-11-07 21:11:52 -0500

    Shadcn Navigation menu component

--- packages/utils/src/shadcn-components/dropdown-menu.tsx
+++
@@ -1,4 +0,0 @@
-import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
-import Link from "next/link";
-import { cn } from "../index";
-import { ChevronDown } from "lucide-react"
