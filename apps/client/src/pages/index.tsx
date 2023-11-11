7500d49bdf4a7256ed32308ec3d1cc2c1bc2a010
Manjinder <msinghw3@gmail.com>
2023-11-10 19:58:04 -0500

    Added registration flow

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -4,15 +4,21 @@ import { ShadcnNav } from "./shadcn-nav-menu";
 import { Skeleton } from "@course-selling-monorepo/utils";
 import { cn } from "@course-selling-monorepo/utils/src/index";
 import { buttonVariants } from "@course-selling-monorepo/utils";
+import RegisterPage from "./register/register-page";
 
 //  ---------  Currently this handler has logic of Appbar  ----------
 export default function Home({ children }: React.PropsWithChildren) {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="container flex h-16 items-center justify-between">
-        <ShadcnNav />
+    <div>
+      <header className="sticky top-0 z-40 w-full border-b bg-background">
+        <div className="container flex h-16 items-center justify-between">
+          <ShadcnNav />
+        </div>
+      </header>
+      <div className="">
+        <RegisterPage />
       </div>
-    </header>
+    </div>
   );
   //const session = useSession();
 }
