49a1a2226c67f3a11cc1371c1c1ed4a45375ba39
Manjinder <msinghw3@gmail.com>
2023-11-25 17:09:26 -0500

    Integrated user session in user-profile-dropdown

--- apps/web/components/user-profile/profile-banner.tsx
+++ apps/web/components/user-profile/profile-banner.tsx
@@ -1,7 +1,27 @@
 import { UserProfileDropdown } from "@course-selling-monorepo/ui";
 import Link from "next/link";
 import { buttonVariants } from "@course-selling-monorepo/utils";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@course-selling-monorepo/auth";
 
-export const ProfileHeader = async () => {
-  return <></>;
+export const ProfileBanner = async () => {
+  const data = await getServerSession(authOptions);
+  const initials = `${data?.user?.name?.charAt(0) ?? ""}`;
+
+  return (
+    <>
+      {data?.user ? (
+        <>
+          <UserProfileDropdown
+            data={data}
+            initials={initials}
+          ></UserProfileDropdown>
+        </>
+      ) : (
+        <Link href={"/sign-in"}>
+          
+        </Link>
+      )}
+    </>
+  );
 };
