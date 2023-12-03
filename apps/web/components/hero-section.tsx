04320f0474a5595139da0253d90795425367ca4a
Manjinder <msinghw3@gmail.com>
2023-12-03 14:55:10 -0500

    Hero section design changes

--- apps/web/components/hero-section.tsx
+++ apps/web/components/hero-section.tsx
@@ -3,26 +3,81 @@ import Image from "next/image";
 
 export const HeroSection = () => {
   return (
-    <section>
-      <div className="container grid grid-cols-1 lg:grid-cols-12 mb-6">
-        <div className="col-span-7">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-              Hello, I'm{" "}
+    <section className="container py-6 lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+
+        <div className="col-span-7 mt-20">
+          <h1 className="mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold text-center">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
+              Find the best Industry ready{" "}
             </span>
           </h1>
         </div>
-        <div className="cols-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+
+        <div className="col-span-5">
+          <div className="flex mb-6">
             <Image
               src="/images/hero-image.png"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="rounded-3xl mr-10"
               alt="Hero Image"
-              width={300}
-              height={300}
+              height={160}
+              width={180}
+              
+            />
+            <Image
+              src="/images/hero-image-sample.png"
+              className="rounded-full"
+              alt="Hero Image"
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className="flex">
+            <Image
+              src="/images/hero-image.png"
+              className="rounded-full mr-10"
+              alt="Hero Image"
+              width={200}
+              height={200}
+            />
+            <Image
+              src="/images/hero-image.png"
+              className="rounded-3xl"
+              alt="Hero Image"
+              width={180}
+              height={180}
             />
           </div>
         </div>
+
+        {/* <div className="cols-span-5 place-self-center mt-4 lg:mt-0">
+            <div className="w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] sm:cols-span-2 ">
+              <Image
+                src="/images/hero-image.png"
+                className="rounded-3xl "
+                alt="Hero Image"
+                width={200}
+                height={200}
+              />
+              <Image
+                src="/images/hero-image-sample.png"
+                className="rounded-3xl "
+                alt="Hero Image"
+                width={200}
+                height={200}
+              />
+            </div>
+
+            <div className="w-[200px] h-[200px] lg:w-[300px] lg:h-[300px] relative">
+              <Image
+                src="/images/hero-image.png"
+                className="rounded-3xl absolute transform -translate-x-1/3 -translate-y-1/3 top-1/3 left-1/3"
+                alt="Hero Image"
+                width={250}
+                height={250}
+              />
+            </div>
+          </div> */}
       </div>
     </section>
   );
