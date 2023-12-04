3a38581550a0c7de09c6515dd628113e87050a2a
Manjinder <msinghw3@gmail.com>
2023-12-03 21:58:58 -0500

    Hero section styling changes

--- apps/web/components/hero-section.tsx
+++ apps/web/components/hero-section.tsx
@@ -10,7 +10,7 @@ export const HeroSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="sm:col-span-12 lg:col-span-6 xl:col-span-7 lg:mt-6 xl:mt-14 mr-5 md:mb-10">
           <h1 className="mb-4 text-4xl lg:text-5xl xl:text-6xl lg:leading-normal font-extrabold lg:text-left ">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-gray-400 to-cyan-800 ">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-gray-400 to-gray-800 ">
               Explore Expert-led Courses for{" "}
             </span>
             <br></br>
@@ -22,9 +22,10 @@ export const HeroSection = () => {
                 1000,
                 "Skill Development",
                 1000,
-                "Knowledge Enhancement",
+                "Knowledge Expansion",
                 1000,
               ]}
+              className="text-cyan-800"
               wrapper="span"
               speed={50}
               repeat={Infinity}
@@ -35,43 +36,84 @@ export const HeroSection = () => {
             opportunity, and unlock your full potential today
           </p>
           <div className="flex">
-            <Button className="mr-2">Test</Button>
-            <Button>Test2</Button>
+            <Button className="mr-2">Sign up</Button>
+            <Button>Sign in</Button>
           </div>
         </div>
 
         <div className="container sm:col-span-12 lg:col-span-6 xl:col-span-5 xl:ml-10 ">
-          <div className="flex mb-8 lg:ml-5 items-center justify-center">
-            <Image
-              src="/images/hero-image.png"
-              className="rounded-3xl mr-10"
-              alt="Hero Image"
-              height={160}
-              width={180}
-            />
-            <Image
-              src="/images/hero-image-sample.png"
-              className="rounded-full"
-              alt="Hero Image"
-              height={200}
-              width={200}
-            />
+          <div className="hidden md:block">
+            <div className="flex mb-8 lg:ml-5 items-center justify-center">
+              <Image
+                src="/images/hero-image.png"
+                className="rounded-3xl mr-10"
+                alt="Hero Image"
+                height={160}
+                width={180}
+              />
+              <Image
+                src="/images/hero-image-sample.png"
+                className="rounded-full md:block"
+                alt="Hero Image"
+                height={200}
+                width={200}
+              />
+            </div>
+            <div className="flex lg:ml-5 items-center justify-center">
+              <Image
+                src="/images/hero-image.png"
+                className="rounded-full mr-10"
+                alt="Hero Image"
+                height={200}
+                width={200}
+              />
+              <Image
+                src="/images/hero-image.png"
+                className="rounded-3xl"
+                alt="Hero Image"
+                height={160}
+                width={180}
+              />
+            </div>
           </div>
-          <div className="flex lg:ml-5 items-center justify-center">
-            <Image
-              src="/images/hero-image.png"
-              className="rounded-full mr-10"
-              alt="Hero Image"
-              height={200}
-              width={200}
-            />
-            <Image
-              src="/images/hero-image.png"
-              className="rounded-3xl"
-              alt="Hero Image"
-              height={160}
-              width={180}
-            />
+
+          <div className="md:hidden mt-8 items-center justify-center">
+            <div className="mb-8">
+              <Image
+                src="/images/hero-image.png"
+                className="rounded-3xl mr-10"
+                alt="Hero Image"
+                height={160}
+                width={180}
+              />
+            </div>
+            <div className="mb-8">
+              <Image
+                src="/images/hero-image-sample.png"
+                className="rounded-full"
+                alt="Hero Image"
+                height={200}
+                width={200}
+              />
+            </div>
+            <div className="mb-8">
+              <Image
+                src="/images/hero-image.png"
+                className="rounded-full mr-10"
+                alt="Hero Image"
+                height={200}
+                width={200}
+              />
+            </div>
+            <div className="ml-4">
+              <Image
+                src="/images/hero-image.png"
+                className="rounded-3xl mr-10"
+                alt="Hero Image"
+                height={160}
+                width={180}
+              />
+            </div>
           </div>
         </div>
       </div>
