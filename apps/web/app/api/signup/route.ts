8c20d9170a57e6dd285838670803ee1f25f73d92
Manjinder <msinghw3@gmail.com>
2023-12-06 17:52:31 -0500

    Added Signup Schema and backend route

---
+++ apps/web/app/api/signup/route.ts
@@ -0,0 +1,24 @@
+import { signUpSchema } from "@/lib/signUpSchema";
+import { NextResponse } from "next/server";
+import { Admin, isDbConnected } from "db";
+
+// Sign-up Route
+export async function POST(request: Request) {
+  await isDbConnected();
+  const body: unknown = await request.json();
+  const parsedInput = signUpSchema.safeParse(body);
+
+  let zodErrors = {};
+  if (!parsedInput.success) {
+    parsedInput.error.issues.forEach((issue) => {
+      zodErrors = { ...zodErrors, [issue.path[0]]: issue.message };
+    });
+  }
+
+  // Sending response back to client
+  return NextResponse.json(
+    Object.keys(zodErrors).length > 0
+      ? { errors: zodErrors }
+      : { success: true }
+  );
+}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
         <ProfileBanner />
       </AppHeader>
       <HomePage />
-
       <Footer />
     </div>
   );
