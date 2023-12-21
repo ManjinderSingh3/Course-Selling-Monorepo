609fbbd2044b1f6d4d9795a58f94715ed59de047
Manjinder <msinghw3@gmail.com>
2023-12-20 23:28:29 -0500

    Removed build step from deplyment

--- apps/web/deploy.sh
+++ apps/web/deploy.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 cd /home/ubuntu/Course-Selling-Monorepo
 git pull origin main
 yarn install
-yarn build
+# yarn build
 pm2 stop next
 pm2 start npm --name "next" --run "start:next"
 # The above command is starting a new PM2 process named "next" and running the NPM script "start:next". 
